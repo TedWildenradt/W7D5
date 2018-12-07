aa6835755633dc18e9e1e51d879ef5401a28c78c
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-07 15:10:08 -0800

    Setup sessions controller

--- benchbnb/app/controllers/api/sessions_controller.rb
+++ benchbnb/app/controllers/api/sessions_controller.rb
@@ -1,10 +1,20 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
 
+    if @user
+      login(@user)
+      render 'api/user/show'
+    else
+      # flash[:errors] = @user.errors.full_messages
+      render json: ["You fucked up"], status: 412
+    end
   end
 
   def destroy
-    if logged_in?
+    @user = current_user
+    if @user
+      logout
       render json: {}
     else
       render json: ["Nobody Logged in"], status: 404
